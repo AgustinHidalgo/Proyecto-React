6b466f54efccd528be75d602c83fbd5189eb3e91
agustinhidalgo10 <agustinhidalgocef18@gmail.com>
2022-12-07 22:13:42 -0300

    commit para entrega

--- src/componentes/producto.js
+++ src/componentes/producto.js
@@ -2,18 +2,21 @@
 
 const Producto = ({id, img, titulo, precio, descript, cantidad, estado}) =>{
     return(
-        <div className="card">
-            <img src={img} alt="..." className="imagen-card" />
-            <h2 className="titulo-card">{titulo}</h2>
-            <p>{descript}</p>
-            <h4 className="precio-card">{precio}</h4>
-            <div>
-                <button className="boton-card">Comprar</button>
-                <span>{cantidad}</span>
-                <span>{estado}</span>
+        
+            <div className="card">
+                <img src={img} alt="..." className="imagen-card" />
+                <h2 className="titulo-card">{titulo}</h2>
+                <p>{descript}</p>
+                <h4 className="precio-card">{precio}</h4>
+                <div className="conteiner-compra">
+                    <button className="boton-card">Comprar</button>
+                    <span>{cantidad}</span>
+                    <span>{estado}</span>
+                </div>
+            
+            
             </div>
         
-        </div>
     )
 }
 
--- src/conteiners/prod-conteiner.js
+++ src/conteiners/prod-conteiner.js
@@ -4,21 +4,22 @@ import {data} from "../utilidad/data"
 const Productos = () =>{
     
     return (
-        <>
-        {
-            data.map(item => {
+        <div className="contenedor-card">
+            {
+                data.map(item => {
+                return(
                 <Producto
                     key={item.id}
-                    
+                    img= {item.img}
                     titulo= {item.titulo}
                     precio= {item.precio}
                     cantidad= {item.cantidad}
                     estado={item.estado} />
-                    })
-                
+                    )
+                })
             }
         
-        </>
+        </div>
     );
 }
 
